21359150af9ea1c1507a164ebb74018effeb5a14
DistronX <ambujgupta2000@gmail.com>
2023-11-27 01:19:53 +0530

    Deleted folder

--- frontend/src/scenes/team/index.jsx
+++ frontend/src/scenes/team/index.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Header from '../../components/Header'
 import { Box } from '@mui/material'
 
-export default function Dashboard() {
+export default function Team() {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Header title="TEAM" subtitle="Team subtitle" />
       </Box>
     </Box>
   )
